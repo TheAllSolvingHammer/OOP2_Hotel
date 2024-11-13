a8b37e064b0af84e45fc4c250cea31a611637d2a
zxvri23 <161262364+zxvri23@users.noreply.github.com>
2024-11-13 22:22:44 +0200

    Added buttons

--- rest/src/main/java/com/tuvarna/hotel/rest/HelloController.java
+++ rest/src/main/java/com/tuvarna/hotel/rest/HelloController.java
@@ -1,14 +1,55 @@
 package com.tuvarna.hotel.rest;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.io.InputStream;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private ImageView imageView;
+    @FXML
+    private TextField textField;
+    @FXML
+    private Button myButton;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+
+    @FXML
+    protected void onImageButtonClick() {
+        if (!imageView.isVisible()) {
+
+            if (imageView.getImage() == null) {
+                InputStream imageStream = getClass().getResourceAsStream("/shrek.jpg");
+                if (imageStream == null) {
+                    System.out.println("Error: Image file not found.");
+                } else {
+                    Image myImage = new Image(imageStream);
+                    imageView.setImage(myImage);
+                }
+            }
+            imageView.setVisible(true);
+        }
+
+    }
+
+    @FXML
+    protected void TextBoxButtonClick() {
+
+        textField.setVisible(true);
+
+        textField.setText("mnogo si qk be!");
+
+
+    }
+
 }
