f7b6de58b94cae6a059d1ad51c09990125032849
Danny <kilatomerat@gmail.com>
2024-11-19 17:52:20 +0200

    Added methods in the repositories

--- persistence/src/main/java/com/tuvarna/hotel/persistence/repositories/ReservationRepository.java
+++ persistence/src/main/java/com/tuvarna/hotel/persistence/repositories/ReservationRepository.java
@@ -1,8 +1,16 @@
 package com.tuvarna.hotel.persistence.repositories;
 
 import com.tuvarna.hotel.persistence.contracts.EntityMarker;
+import com.tuvarna.hotel.persistence.entities.HotelEntity;
+import com.tuvarna.hotel.persistence.entities.ReservationEntity;
+import com.tuvarna.hotel.persistence.entities.RoomEntity;
 
+import java.sql.Timestamp;
+import java.util.List;
 import java.util.UUID;
 
 public interface ReservationRepository<T extends EntityMarker,E extends UUID> extends BaseRepository<T, E> {
+    List<RoomEntity> getAllFreeRooms(HotelEntity hotel, Timestamp startDate, Timestamp endDate);
+    Long getLastReservationNumberOfHotel(HotelEntity hotel);
+    ReservationEntity getReservationWithNumber(Long reservationNumber, HotelEntity hotel);
 }
