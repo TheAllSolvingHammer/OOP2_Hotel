f05efe3efaef1abc60b9a5649a9d2d834103d66b
Yordan Simeonov <161168617+TheAllSolvingHammer@users.noreply.github.com>
2024-11-19 17:53:16 +0200

    Added methods in the repositories (#31)

--- persistence/src/main/java/com/tuvarna/hotel/persistence/daos/ReservationRepositoryImpl.java
+++ persistence/src/main/java/com/tuvarna/hotel/persistence/daos/ReservationRepositoryImpl.java
@@ -1,8 +1,15 @@
 package com.tuvarna.hotel.persistence.daos;
 
+import com.tuvarna.hotel.persistence.connection.HibernateUtil;
+import com.tuvarna.hotel.persistence.entities.HotelEntity;
 import com.tuvarna.hotel.persistence.entities.ReservationEntity;
+import com.tuvarna.hotel.persistence.entities.RoomEntity;
 import com.tuvarna.hotel.persistence.repositories.ReservationRepository;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
+import java.sql.Timestamp;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -21,11 +28,91 @@ public class ReservationRepositoryImpl implements ReservationRepository<Reservat
 
     @Override
     public ReservationEntity save(ReservationEntity reservationEntity) {
-        return null;
+        Session session = HibernateUtil.openSession();
+        try (session) {
+            Transaction transaction = session.beginTransaction();
+            session.persist(reservationEntity);
+            transaction.commit();
+            return reservationEntity;
+        } catch (Exception e) {
+            System.out.println("Error in creation");
+            return null;
+        }
     }
 
     @Override
     public ReservationEntity delete(ReservationEntity reservationEntity) {
-        return null;
+        Session session = HibernateUtil.openSession();
+        Transaction transaction = session.beginTransaction();
+        try {
+            session.remove(reservationEntity);
+            transaction.commit();
+            return reservationEntity;
+        } catch (Exception e) {
+            System.out.println("Error in creation");
+            return null;
+        } finally {
+            session.close();
+        }
+    }
+
+    @Override
+    public List<RoomEntity> getAllFreeRooms(HotelEntity hotel, Timestamp startDate, Timestamp endDate) {
+        Session session = HibernateUtil.openSession();
+        Transaction transaction = session.beginTransaction();
+        List<RoomEntity> rooms = new LinkedList<>();
+        try{
+            String jpql = "SELECT r FROM Rooms r " +
+                    "LEFT JOIN reservations rr ON (rr.room.id = r.id " +
+                    "AND NOT ( " +
+                    "(rr.startDate < '"+startDate+"' AND rr.endDate < '"+startDate+"') " +
+                    "OR (rr.startDate > '"+endDate+"' AND rr.endDate > '"+endDate+"'))) " +
+                    "WHERE rr.room.id IS NULL AND r.hotel.id = '"+hotel.getId()+"' AND r.number != '"+13+"'" +
+                    "ORDER BY r.number ASC";
+
+            rooms.addAll(session.createQuery(jpql, RoomEntity.class).getResultList());
+            transaction.commit();
+
+        } catch (Exception e) {
+
+        } finally {
+            session.close();
+        }
+        return rooms;
+    }
+
+    @Override
+    public Long getLastReservationNumberOfHotel(HotelEntity hotel) {
+        Session session = HibernateUtil.openSession();
+        Transaction transaction = session.beginTransaction();
+        Long lastReservationNumber = null;
+        try{
+            String jpql = "SELECT MAX (rr.number)  FROM Reservations rr WHERE rr.hotel.id = '"+hotel.getId()+"'";
+            lastReservationNumber = (Long) session.createQuery(jpql).getSingleResult();
+            transaction.commit();
+
+        } catch (Exception e) {
+
+        } finally {
+            session.close();
+        }
+        return lastReservationNumber;
+    }
+
+    @Override
+    public ReservationEntity getReservationWithNumber(Long reservationNumber, HotelEntity hotel) {
+        Session session = HibernateUtil.openSession();
+        Transaction transaction = session.beginTransaction();
+        ReservationEntity reservation = null;
+        try{
+            String jpql = "SELECT rr FROM Reservations rr WHERE rr.number = '"+reservationNumber+"' AND rr.hotel = '"+ hotel.getId() +"' AND ROWNUM <=1 ORDER BY rr.room.number ";
+            reservation = (ReservationEntity) session.createQuery(jpql).getSingleResult();
+            transaction.commit();
+
+        } catch (Exception e) {
+        } finally {
+            session.close();
+        }
+        return reservation;
     }
 }
