9a7fe17b8338050ffe25326fe44d3c4de117a9e4
zxvri23 <161262364+zxvri23@users.noreply.github.com>
2024-12-29 17:37:46 +0200

    Creating clients. (#56)

---
+++ rest/src/main/java/com/tuvarna/hotel/rest/controllers/reception/clients/ClientAdd.java
@@ -0,0 +1,96 @@
+package com.tuvarna.hotel.rest.controllers.reception.clients;
+
+import com.tuvarna.hotel.api.exceptions.ErrorProcessor;
+import com.tuvarna.hotel.api.models.create.client.CreateClientInput;
+import com.tuvarna.hotel.api.models.create.client.CreateClientOutput;
+import com.tuvarna.hotel.core.processes.CreateClientProcess;
+import com.tuvarna.hotel.domain.singleton.SingletonManager;
+import com.tuvarna.hotel.rest.alert.AlertManager;
+import io.vavr.control.Either;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+public class ClientAdd {
+    @FXML
+    public TextField firstName;
+    @FXML
+    public TextField lastName;
+    @FXML
+    public TextField phone;
+    @FXML
+    public TextField ucn;
+    @FXML
+    public TextField address;
+    @FXML
+    public TextField email;
+    @FXML
+    public TextField birthDate;
+    @FXML
+    public TextField issueDate;
+    @FXML
+    public TextField expireDate;
+    @FXML
+    public TextField issuedBy;
+
+    private Stage stage;
+    private Parent root;
+    private Scene scene;
+
+    private final CreateClientProcess createClientProcess = SingletonManager.getInstance(CreateClientProcess.class);
+
+    @FXML
+    public void switchToBeginning(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("/com/tuvarna/hotel/rest/reception/receptionist-view.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Manager");
+        stage.show();
+    }
+
+    public void addClient(ActionEvent event) {
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
+        try {
+            CreateClientInput input = CreateClientInput.builder()
+                    .firstName(firstName.getText())
+                    .lastName(lastName.getText())
+                    .phone(phone.getText())
+                    .ucn(ucn.getText())
+                    .address(address.getText())
+                    .email(email.getText())
+                    .birthDate(LocalDate.parse(birthDate.getText(), dateFormatter)) // Using custom formatter
+                    .issueDate(LocalDate.parse(issueDate.getText(), dateFormatter)) // Using custom formatter
+                    .expireDate(LocalDate.parse(expireDate.getText(), dateFormatter)) // Using custom formatter
+                    .issuedBy(issuedBy.getText())
+                    .build();
+
+            Either<ErrorProcessor, CreateClientOutput> result = createClientProcess.process(input);
+
+            result.fold(
+                    error -> {
+                        AlertManager.showAlert(Alert.AlertType.ERROR, "Error in creating client", error.getMessage());
+                        return null;
+                    },
+                    success -> {
+                        AlertManager.showAlert(Alert.AlertType.CONFIRMATION, "Client created", success.getMessage());
+                        return null;
+                    }
+            );
+        } catch (DateTimeParseException e) {
+            AlertManager.showAlert(Alert.AlertType.ERROR, "Invalid Date Format", "Please enter the date in the format dd.MM.yyyy");
+        }
+    }
+}
