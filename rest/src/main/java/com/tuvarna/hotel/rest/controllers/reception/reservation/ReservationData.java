7c159a2107c3d381374163e2f687b49ac6d12ff6
Danny <kilatomerat@gmail.com>
2024-12-30 00:54:13 +0200

    Fixes dues to merging

---
+++ rest/src/main/java/com/tuvarna/hotel/rest/controllers/reception/reservation/ReservationData.java
@@ -0,0 +1,38 @@
+package com.tuvarna.hotel.rest.controllers.reception.reservation;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ReservationData {
+    private Stage stage;
+    private Parent root;
+    private Scene scene;
+
+    @FXML
+    protected void switchToBeginning(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("/com/tuvarna/hotel/rest/reception/receptionist-view.fxml"));
+        stage=(Stage)((Node)event.getSource()).getScene().getWindow();
+        scene=new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Receptionist View");
+        stage.show();
+    }
+
+    @FXML
+    private void switchToTable(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("/com/tuvarna/hotel/rest/reception/new-reservation.fxml"));
+        stage=(Stage)((Node)event.getSource()).getScene().getWindow();
+        scene=new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Navigation");
+        stage.show();
+    }
+
+}
