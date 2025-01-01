169dd89e42003b6f1e4d85cc567f5591306e2400
zxvri23 <161262364+zxvri23@users.noreply.github.com>
2025-01-01 18:03:25 +0200

    Touching up search bars and added a column for the rating of clients. (#71)

--- api/src/main/java/com/tuvarna/hotel/api/models/query/client/information/ClientInformationInput.java
+++ api/src/main/java/com/tuvarna/hotel/api/models/query/client/information/ClientInformationInput.java
@@ -1,11 +1,8 @@
 package com.tuvarna.hotel.api.models.query.client.information;
 
 import com.tuvarna.hotel.api.base.OperationInput;
-import jakarta.validation.constraints.*;
 import lombok.*;
 
-import java.time.LocalDate;
-import java.util.UUID;
 
 @Getter
 @Setter
